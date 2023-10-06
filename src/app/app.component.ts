47581babd5f7e44361585c30169a01e7926266e9
dijithdamodaran <dijithdamodaran@gmail.com>
2023-10-06 12:15:44 +0530

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   userEmail:string="dij@gmail.com";
   userAge:number=30;
   userCompany:string="it-vedant";
-  
+  imageurl:string="assets/image.jpg";
   data : string = ""
   onsave(){
     this.data = this.userName + ":" + this.userAge + ":" + this.userCompany + ":" + this.userEmail;
